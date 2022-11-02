e3bcc6c912394700e274887675479234d75be863
Andreea <nenciucrasi.andreea@gmail.com>
2022-11-02 13:02:03 +0200

    adds basic page layout

--- backend/views/layouts/auth.php
+++ backend/views/layouts/auth.php
@@ -14,12 +14,7 @@ $this->beginContent('@backend/views/layouts/base.php');
 ?>
 
 
-
-
     <main role="main" class="d-flex">
-
-        <?php echo $this->render('_sidebar') ?>
-
         <div class="content-wrapper p-3">
             <?= Breadcrumbs::widget([
                 'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
